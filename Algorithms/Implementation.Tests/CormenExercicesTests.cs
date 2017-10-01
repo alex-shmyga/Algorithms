7f716921af8eff64fda9550b80849b69d7caaab2
Alexander Shmyga <shmyga.87@gmail.com>
2017-10-01 23:58:16 +0200

    exercise from Kormen book. ch2 ex1

---
+++ Algorithms/Implementation.Tests/CormenExercicesTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+using Implementation.KormanExercices;
+using Xunit;
+using System.Linq;
+
+namespace Implementation.Tests
+{
+    public class CormenExercicesTests
+    {
+        [Fact]
+        public void SmartMergeSort_sorts_unsorted_array()
+        {
+            // arrange
+            var array = GetRandomIntArray(100000);
+
+            // act
+            Solutions.SmartMergeSort(array);
+
+            // assert
+            array.Should().BeInAscendingOrder();
+        }
+
+        private int[] GetRandomIntArray(int size)
+        {
+            int Min = Int32.MinValue;
+            int Max = Int32.MaxValue;
+            var randNum = new Random();
+            int[] arr = Enumerable
+                .Repeat(0, size)
+                .Select(i => randNum.Next(Min, Max))
+                .ToArray();
+
+            return arr;
+        }
+    }
+}
---
+++ Algorithms/Implementation/CormenExercices/Solutions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.KormanExercices
+{
+    public class Solutions
+    {
+        private static void Merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int ii = 0; ii < n1; ++ii)
+                L[ii] = arr[l + ii];
+            for (int jj = 0; jj < n2; ++jj)
+                R[jj] = arr[m + 1 + jj];
+
+            int i = 0, j = 0;
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        private static void SmartMergeSort(int[] arr, int l, int r, int k)
+        {
+            if (arr.Length <= k)
+            {
+                Sorting.InsertionSort(arr);
+                return;
+            }
+
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+
+                SmartMergeSort(arr, l, m, k);
+                SmartMergeSort(arr, m + 1, r, k);
+                Merge(arr, l, m, r);
+            }
+        }
+
+        public static void SmartMergeSort(int[] arr)
+        {
+            int k = GetK();
+            SmartMergeSort(arr, 0, arr.Length - 1, k);
+        }
+
+        private static int GetK()
+        {
+            int k = 2;
+            while (Math.Pow(k, 2) < (k * Math.Log(2, k)))
+            {
+                k = k * 2;
+            }
+
+            return k;
+        }
+    }
+}
