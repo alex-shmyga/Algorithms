bdb430e42bc5322619006f61a9dc9c8e78f91e56
Alexander Shmyga <shmyga.87@gmail.com>
2017-10-01 12:01:40 +0200

    InsertionSort added
    #ncrunch added
    #xUnit added

--- Algorithms/Implementation.Tests/Sorting.Tests.cs
+++ Algorithms/Implementation.Tests/Sorting.Tests.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Implementation.Tests
 {
     public class SortingTests
     {
+        [Fact]
+        public void InsertionSort_sorts_unsorted_array()
+        {
+            // arrange
+            var unsortedArray = new int [] { 5, 9, 4, 1, 7 };
+
+            // act
+            var sortedArray = Sorting.InsertionSort(unsortedArray);
+
+            // assert
+            sortedArray.Should().BeInAscendingOrder();
+        }
+
+
+        [Fact]
+        public void InsertionSortDesc_sorts_unsorted_array_in_decremental_order()
+        {
+            // arrange
+            var unsortedArray = new int[] { 5, 9, 4, 1, 7 };
+
+            // act
+            var sortedArray = Sorting.InsertionSortDesc(unsortedArray);
+
+            // assert
+            sortedArray.Should().BeInDescendingOrder();
+        }
     }
 }
--- Algorithms/Implementation/Sorting.cs
+++ Algorithms/Implementation/Sorting.cs
@@ -8,5 +8,47 @@ namespace Implementation
 {
     public class Sorting
     {
+        #region Worst-case performance: О(n2)
+
+        public static int[] InsertionSort(int[] ar)
+        {
+            for (int j = 1; j <= ar.Length - 1; j++)
+            {
+                int key = ar[j];
+
+                int i = j - 1;
+                while (i >= 0 && ar[i] > key)
+                {
+                    ar[i + 1] = ar[i];
+                    i = i - 1;
+                }
+
+                ar[i + 1] = key;
+            }
+
+            return ar;
+        }
+
+        public static int[] InsertionSortDesc(int[] ar)
+        {
+            for (int j = 1; j <= ar.Length - 1; j++)
+            {
+                int key = ar[j];
+
+                int i = j - 1;
+                while (i >= 0 && ar[i] < key)
+                {
+                    ar[i + 1] = ar[i];
+                    i = i - 1;
+                }
+
+                ar[i + 1] = key;
+            }
+
+            return ar;
+        }
+
+
+        #endregion
     }
 }
