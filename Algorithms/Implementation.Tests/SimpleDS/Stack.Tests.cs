c28614fc12578015415809d2316b5d49e15a27b5
Alexander Shmyga <shmyga.87@gmail.com>
2017-10-30 11:30:32 +0100

    Basic Stack implementation added

---
+++ Algorithms/Implementation.Tests/SimpleDS/Stack.Tests.cs
@@ -0,0 +1,40 @@
+﻿using Implementation.SimpleDS;
+using Xunit;
+using FluentAssertions;
+
+namespace Implementation.Tests.SimpleDS
+{
+    public class StackTests
+    {
+        [Fact]
+        //public void Pop_should_return_latest_element()
+        public void Push_should_add_element()
+        {
+            // arrange
+            var stack = new Stack();
+
+            // act
+            stack.Push(5);
+
+            // assert
+            stack.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void Pop_should_return_latest_element()
+        {
+            // arrange
+            var stack = new Stack();
+            stack.Push(5);
+            stack.Push(7);
+            stack.Push(3);
+
+            // act
+            var result = stack.Pop();
+
+            // assert
+            result.Should().Be(3);
+            stack.Count.Should().Be(2);
+        }
+    }
+}
---
+++ Algorithms/Implementation/SimpleDS/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Implementation.SimpleDS
+{
+    public class Stack
+    {
+        private int[] _arr;
+        int top = -1;
+
+        public Stack()
+        {
+            _arr = new int[2];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return top + 1;
+            }
+        }
+
+        public void Push(int x)
+        {
+            if (_arr.Length == top + 1)
+            {
+                int[] newArray = new int[_arr.Length * 2];
+                Array.Copy(_arr, 0, newArray, 0, _arr.Length);
+                _arr = newArray;
+                newArray = null;
+            }
+            top++;
+            _arr[top] = x;
+        }
+
+        private bool StackEmpty()
+        {
+            return (top == -1);
+        }
+
+        public int Pop()
+        {
+            if (StackEmpty())
+            {
+                throw new Exception("underflow");
+            }
+
+            return _arr[top--];
+        }
+    }
+}
