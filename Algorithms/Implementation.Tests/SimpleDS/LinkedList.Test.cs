c7ea492216af739a2f874c6164924eed5f5bfd1e
Alexander Shmyga <shmyga.87@gmail.com>
2017-10-31 12:19:00 +0100

    very basic implementation of DoubleLinkedList added

---
+++ Algorithms/Implementation.Tests/SimpleDS/LinkedList.Test.cs
@@ -0,0 +1,71 @@
+﻿using Implementation.SimpleDS;
+using Xunit;
+using FluentAssertions;
+
+namespace Implementation.Tests.SimpleDS
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void Insert_should_add_element()
+        {
+            // arrange
+            var list = new LinkedList();
+
+            // act
+            list.Insert(5);
+
+            // assert
+            list.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void Insert_should_add_element2()
+        {
+            // arrange
+            var list = new LinkedList();
+
+            // act
+            list.Insert(5);
+            list.Insert(7);
+            list.Insert(10);
+
+            // assert
+            list.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void Search_should_return_asking_node()
+        {
+            // arrange
+            var list = new LinkedList();
+            list.Insert(5);
+            list.Insert(7);
+            list.Insert(10);
+
+            // act
+            var result = list.Search(7);
+
+            // assert
+            result.key.Should().Be(7);
+        }
+
+        [Fact]
+        public void Delete_should_remove_asking_node()
+        {
+            // arrange
+            var list = new LinkedList();
+            list.Insert(5);
+            list.Insert(7);
+            list.Insert(10);
+
+            // act
+            list.Remove(7);
+            var result = list.Search(7);
+
+            // assert
+            result.Should().BeNull();
+            list.Count.Should().Be(2);
+        }
+    }
+}
---
+++ Algorithms/Implementation/SimpleDS/LinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Implementation.SimpleDS
+{
+    public class LinkedList
+    {
+        Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return getLength();
+            }
+        }
+
+
+        private int getLength()
+        {
+            var node = head;
+
+            int c = 0;
+            while (node != null)
+            {
+                c++;
+                node = node.next;
+            }
+
+            return c;
+        }
+
+        public void Insert(int key)
+        {
+            var newNode = new Node(key);
+            newNode.next = head;
+
+            if (head != null)
+            {
+                head.prev = newNode;
+            }
+
+            head = newNode;
+            newNode.prev = null;
+        }
+
+        public Node Search(int k)
+        {
+            var node = head;
+
+            while (node != null && node.key != k)
+            {
+                node = node.next;
+            }
+
+            return node;
+        }
+
+        public void Remove(int k)
+        {
+            var node = Search(k);
+
+            if (node.prev != null)
+            {
+                node.prev.next = node.next;
+            }
+            else
+            {
+                head = node.next;
+            }
+
+            if (node.next != null)
+            {
+                node.next.prev = node.prev;
+            }
+        }
+    }
+
+    public class Node
+    {
+        public int key;
+        public Node prev;
+        public Node next;
+
+        public Node(int x)
+        {
+            key = x;
+        }
+    }
+}
