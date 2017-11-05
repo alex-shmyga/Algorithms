1accb7b4b402121dc5a5d86eae50677f1df09322
Alexander Shmyga <shmyga.87@gmail.com>
2017-11-05 22:18:19 +0100

    Basic operation with BinaryTree added

---
+++ Algorithms/Implementation.Tests/SimpleDS/BinaryTree.Tests.cs
@@ -0,0 +1,68 @@
+﻿using Implementation.SimpleDS;
+using Xunit;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace Implementation.Tests.SimpleDS
+{
+    public class BinaryTreeTests
+    {
+        [Fact]
+        public void InorderTreeWalk_goes_through_tree_in_an_ascending_order()
+        {
+            // arrange
+            var head = new BTNode() { key = 5 };
+            head.left = new BTNode() { key = 4 };
+            head.right = new BTNode() { key = 7 };
+            head.left.left = new BTNode() { key = 3 };
+
+            var printedValues = new Queue<int>();
+
+            // act
+            BinaryTree.InorderTreeWalk(head, (int value) => printedValues.Enqueue(value));
+
+
+            // assert
+            printedValues.Should().BeEquivalentTo(new[] {3, 4, 5, 7 });
+        }
+
+        [Fact]
+        public void PreoderTreeWalk_goes_through_tree_starting_from_the_root()
+        {
+            // arrange
+            var head = new BTNode() { key = 5 };
+            head.left = new BTNode() { key = 4 };
+            head.right = new BTNode() { key = 7 };
+            head.left.left = new BTNode() { key = 3 };
+
+            var printedValues = new Queue<int>();
+
+            // act
+            BinaryTree.PreoderTreeWalk(head, (int value) => printedValues.Enqueue(value));
+
+
+            // assert
+            printedValues.Should().BeEquivalentTo(new[] { 5, 4, 7, 3 });
+        }
+
+
+        [Fact]
+        public void PostoderTreeWalk_goes_through_tree_in_a_descending_order()
+        {
+            // arrange
+            var head = new BTNode() { key = 5 };
+            head.left = new BTNode() { key = 4 };
+            head.right = new BTNode() { key = 7 };
+            head.left.left = new BTNode() { key = 3 };
+
+            var printedValues = new Queue<int>();
+
+            // act
+            BinaryTree.PostoderTreeWalk(head, (int value) => printedValues.Enqueue(value));
+
+
+            // assert
+            printedValues.Should().BeEquivalentTo(new[] { 7, 5, 4, 3 });
+        }
+    }
+}
---
+++ Algorithms/Implementation/SimpleDS/BinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Implementation.SimpleDS
+{
+    public static class BinaryTree
+    {
+        public static void InorderTreeWalk(this BTNode node, Action<int> func)
+        {
+            if (node != null)
+            {
+                InorderTreeWalk(node.left, func);
+                func(node.key);
+                InorderTreeWalk(node.right, func);
+            }
+        }
+
+        public static void PreoderTreeWalk(this BTNode node, Action<int> func)
+        {
+            if (node != null)
+            {
+                func(node.key);
+                PreoderTreeWalk(node.left, func);
+                PreoderTreeWalk(node.right, func);
+            }
+        }
+
+        public static void PostoderTreeWalk(this BTNode node, Action<int> func)
+        {
+            if (node != null)
+            {
+                PostoderTreeWalk(node.left, func);
+                PostoderTreeWalk(node.right, func);
+                func(node.key);
+            }
+        }
+    }
+
+
+    public class BTNode
+    {
+        public int key;
+        public BTNode parent;
+        public BTNode left;
+        public BTNode right;
+    }
+}
