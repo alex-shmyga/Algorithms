6e5fd501199520dfa786671c4b9108f6a334a45b
Alexander Shmyga <shmyga.87@gmail.com>
2017-11-29 17:43:10 +0100

    The Coin Change Problem from hackerrank added

---
+++ Algorithms/Implementation.Tests/DynamicProgramming/Exercices.Tests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Implementation.DynamicProgramming.Exercices;
+using Xunit;
+
+namespace Implementation.Tests.DynamicProgramming
+{
+    public class ExercicesTests
+    {
+        [Fact]
+        public void TheCoinChangeProblem_test()
+        {
+            // arrange
+            long[] c = new long[] { 1, 2, 3 };
+
+            // act
+            long result = TheCoinChangeProblem.GetWays(4, c);
+
+            // assert
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void TheCoinChangeProblem_test_2()
+        {
+            // arrange
+            long[] c = new long[] { 2, 5, 3, 6 };
+
+            // act
+            long result = TheCoinChangeProblem.GetWays(10, c);
+
+            // assert
+            result.Should().Be(5);
+        }
+    }
+}
---
+++ Algorithms/Implementation/DynamicProgramming/Exercices/TheCoinChangeProblem.cs
@@ -0,0 +1,20 @@
+﻿namespace Implementation.DynamicProgramming.Exercices
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/coin-change/problem
+    /// </summary>
+    public class TheCoinChangeProblem
+    {
+        public static long GetWays(int n, long[] p)
+        {
+            long[] memo = new long[n + 1];
+            memo[0] = 1;
+
+            for (long j = 0; j < p.Length; j++)
+                for (long i = p[j]; i <= n; i++)
+                    memo[i] += memo[i - p[j]];
+
+            return memo[n];
+        }
+    }
+}
